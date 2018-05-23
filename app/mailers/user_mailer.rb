86c0211d5fb1acc876797c9b940426f1a97f0c4e
Axel <axelbruget@gmail.com>
2018-05-23 14:51:46 +0200

    Changed email address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def inform_admin(current_user,text)
     @user = current_user
     @text = text
-    mail(to: 'alexandre.cuer@cerema.fr', subject: 'Activity report'.to_s.humanize) do |format|
+    mail(to: 'axelbruget@gmail.com', subject: 'Activity report'.to_s.humanize) do |format|
         format.html
     end
   end
