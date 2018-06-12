11a6a033d6a5b508ba428d8a7365737eb5c92429
Axel <axelbruget@gmail.com>
2018-06-12 14:36:27 +0200

    Cleaning merge

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,46 +1,17 @@
 class PollsController < ApplicationController
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   before_action :authenticate_user!, :check_admin
 
   def check_admin
     if !current_user.is_admin?
       flash[:notice] = "Vous n'avez pas les droits administrateurs" 
       redirect_to root_url
-=======
-    before_action :authenticate_user!, :check_admin
-
-=======
-    before_action :authenticate_user!, :check_admin
-
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
-    def check_admin
-        if !current_user.is_admin?
-            flash[:notice] = "Vous n'avez pas les droits administrateurs"
-            redirect_to root_url
-        end
-    end
-
- 	def index 
- 	    @poll = Poll.find_by_id(params[:id])
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
     end
   end
 
-<<<<<<< HEAD
   def index
     @poll = Poll.find_by_id(params[:id])
   end
-=======
-    def edit
-        @poll = Poll.find_by_id(params[:id])
-        if !@poll
-            flash[:notice] = "Vous ne pouvez pas apporter de modifications à un sondage qui n'existe pas"
-    	    redirect_to root_url
-        end
-    end
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
 
   def edit
     @poll = Poll.find_by_id(params[:id])
@@ -50,8 +21,6 @@ class PollsController < ApplicationController
     end
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   def update
     @poll = Poll.find_by_id(params[:id])
     array = poll_params[:closed_names].split(";") + poll_params[:open_names].split(";")
@@ -84,51 +53,6 @@ class PollsController < ApplicationController
       format.csv { send_data @poll.to_csv(@hash), filename: "polls-#{Date.today}.csv" }
     end
   end
-=======
-    def new
-        @poll = current_user.polls.new
-    end
-
-=======
-    def new
-        @poll = current_user.polls.new
-    end
-
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
-    def show
-        @poll = Poll.find_by_id(params[:id])
-        @hash = current_user.get_all_emails
-        if !@poll
-            flash[:notice] = "Ce numéro de sondage n'existe pas"
-            redirect_to root_url
-        end
-        
-        respond_to do |format|
-            format.html
-            format.csv { send_data @poll.to_csv(@hash), filename: "polls-#{Date.today}.csv" }
-        end
-    end
-
-    def create
-        @poll = current_user.polls.new(poll_params)
-        # le formulaire est crée seulement si on a un titre, une description et au moins une question ( ouverte ou fermée )
-        if (( @poll.open_names == "" && @poll.closed_names == "" ) || @poll.description == "" || @poll.name == "" )
-            flash[:notice] = "Il manque des champs obligatoires dans le formulaire"
-        else
-            array = @poll.get_names
-            if array.uniq.count != array.size
-                flash[:notice] = "Il n'est pas possible de créer un formulaire avec des questions identiques"
-            else
-                @poll.save
-                flash[:notice] = "Nouveau formulaire crée avec succès"
-            end
-        end
-        redirect_to root_url
-    end
-<<<<<<< HEAD
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
-=======
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
 
   def create
     @poll = current_user.polls.new(poll_params)
@@ -144,7 +68,6 @@ class PollsController < ApplicationController
         flash[:notice] = "Nouveau formulaire crée avec succès"
       end
     end
-<<<<<<< HEAD
     redirect_to root_url
   end
 
@@ -153,28 +76,14 @@ class PollsController < ApplicationController
     Folder.where(poll_id: @poll.id).each do |f|
       f.poll_id = nil
       f.save
-=======
-  
-    private
-    def poll_params
-	  params.require(:poll).permit(:name, :description, :closed_names, :open_names, :closed_names_number, :open_names_number)
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
     end
     @poll.destroy
     flash[:notice] = "Suppression du formulaire réussie"
     redirect_to root_url
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   private
   def poll_params
     params.require(:poll).permit(:name, :description, :closed_names, :open_names, :closed_names_number, :open_names_number)
   end
 end
-=======
-end
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
-=======
-end
->>>>>>> 919a09ff0356b499e2a673999ef00c1f9515df9b
