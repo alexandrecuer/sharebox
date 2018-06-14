a5a7db89ea2347c3959eab5e714a6d7e9158f156
Axel <axelbruget@gmail.com>
2018-06-14 14:50:35 +0200

    version v0.1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,17 +3,14 @@ class UserMailer < ApplicationMailer
   def inform_admin(current_user,text)
     @user = current_user
     @text = text
-    mail(to: User.find_by_id(1).email, subject: 'Activity report'.to_s.humanize) do |format|
-      format.html
-    end
+    #attachments.inline["logo.jpg"] = File.read("#{Rails.root}/app/assets/images/logo.jpg")
+    mail(to: User.find_by_id(1).email, subject: 'Activity report')
   end
 
   def inform_user(email)
     @email = email
     @user = User.find_by_email(email)
-
-    mail(to: email, subject: 'Votre livrable est disponible en ligne'.to_s.humanize) do |format|
-      format.html
-    end
+    #attachments.inline["logo.jpg"] = File.read("#{Rails.root}/app/assets/images/logo.jpg")
+    mail(to: email, subject: 'Votre livrable est disponible en ligne')
   end
 end
