83de41d39077ce2653421f47d951db4e74807b4d
Alexandre CUER <alexandre.cuer@cerema.fr>
2018-08-19 19:19:54 +0200

    multilang_customization_1

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,28 +1,32 @@
 ## 
-# manage assets creation inside folders within the sharebox site
+# manage assets'creation inside the sharebox site
 
 class AssetsController < ApplicationController
 
 before_action :authenticate_user!
   
-  ## A view accessible via "/assets/assets_id"
-  # Show the name of the file and its directory (forge/attachments//asset_id/asset_name)
+  ## 
+  # Method used when following the route /assets/assets_id<br>
+  # Show the name of the file and its directory (forge/attachments/asset_id/asset_name)
   def show
     @asset = current_user.assets.find(params[:id])
   end
   
   ##
-  # Show the new form in order to upload a new asset
-  # Only admin or private users are able to download files on the application
-  # They cannot upload files inside folder they don't own
+  # Show the new form in order to upload a new asset<br>
+  # method used when following a route /folders/folder_id/new_file or /assets/new<br>
+  # /folders/folder_id/new_file will upload the asset in the folder identified by folder_id<br>
+  # /assets/new will upload the asset at the root of the user - such files are strictly personal and cannot be shared<br>  
+  # Only admin or private users are able to upload files<br>
+  # They cannot upload files outside the folders they own
   def new
     if !(current_user.is_admin? || current_user.is_private?)
       flash[:notice] = ASSETS_MSG["rights_missing"]
       redirect_to root_url
     end
     @asset = current_user.assets.new
-    # If there is a folder_id, then the path will be like : /folders/folder_id/new_file
-    # Else the file will be located at the root 
+    # If there is a folder_id, we attach the file to the corresponding folder<br>
+    # if not, ir will be a root located file 
     if params[:folder_id]
       @current_folder = Folder.find_by_id(params[:folder_id])
       if @current_folder
@@ -39,9 +43,8 @@ before_action :authenticate_user!
   end
   
   ##
-  # Upload an asset and register it in the database
-  # if the asset is located at the root, we redirect to root 
-  # else we redirect to the parent folder of the file
+  # following the call to the new asset method, upload an asset and register it in the database<br>
+  # if the asset is a root file, we redirect to root else we redirect to the parent folder
   def create
 	  @asset = current_user.assets.new(asset_params)
       if @asset.save
@@ -60,8 +63,8 @@ before_action :authenticate_user!
   end
   
   ##
-  # Destroy the asset 
-  # files can be deleted by their owners
+  # Destroy the asset<br>
+  # only for owners - to be fixed
   def destroy
     @asset = current_user.assets.find(params[:id])
     @asset.destroy
@@ -75,10 +78,11 @@ before_action :authenticate_user!
   
 
   ##
-  # Permits the user to download a file
-  # there is 2 different options :
-  # - 1) The file is stored locally in a directory egg : (forge/attachments//asset_id/asset_name)
-  # - 2) Amazon S3 mode : The file is stored in a cloud storage
+  # Permits the user to download a file<br>
+  # 2 different options for file storage are possible :<br>
+  # - 1) local storage in application_root/forge/attachments/<br>
+  # - 2) Amazon S3 mode, in a cloud storage<br>
+  # asset will be stored in a directory named with the asset id : asset_id/asset_name
   def get
     #asset = current_user.assets.find_by_id(params[:id])
     asset = Asset.find_by_id(params[:id])
@@ -87,7 +91,7 @@ before_action :authenticate_user!
       #case 1 : asset is a root file
       if !asset.folder_id
         if current_user.has_asset_ownership?(asset)
-          # switching to S3, we use "redirect_to asset.uploaded_file.expiring_url(10)""
+          # switching to S3, we use "redirect_to asset.uploaded_file.expiring_url(10)"
           # this creates a valid 10s url that allows access to private S3 files
           #send_file asset.uploaded_file.path, :type => asset.uploaded_file_content_type
           redirect_to asset.uploaded_file.expiring_url(10)
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,10 +8,10 @@ class HomeController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   ## 
-  # Admin users can modify other users'status
-  # 3 differents status are public, private and admin. 
-  # The first user registered on the application is considered like a super admin, his status cannot be changed
-  # An admin cannot change his own status 
+  # Admin users can modify other users'status<br>
+  # The 3 different status are public, private and admin<br> 
+  # The first user registered on the application is considered like a super admin, his status is timeless and cannot be changed<br>
+  # An admin cannot change his own status<br>
   def update
     if !current_user.is_admin?
       flash[:notice] = HOME_MSG["only_for_admin"]
@@ -32,12 +32,12 @@ class HomeController < ApplicationController
           if change_statut == 1
             @user.statut = params[:statut]
             if @user.save
-              flash[:notice] = @user.email + '('+@user.id.to_s+') a désormais le statut :'+@user.statut
+              flash[:notice] = @user.email + '('+@user.id.to_s+')'+HOME_MSG["new_status"]+@user.statut
             else
               flash[:notice] = HOME_MSG["error_changing_status"]
             end
           else
-            flash[:notice] = HOME_MSG["own_statut_nor_superadmin_cannot_be_changed"]
+            flash[:notice] = HOME_MSG["own_status_nor_superadmin_cannot_be_changed"]
           end
         else
           flash[:notice] = HOME_MSG["inexisting_user"]
@@ -50,7 +50,7 @@ class HomeController < ApplicationController
   end
     
   ##
-  # Show a complete view of all users registered on the application
+  # Show a complete view of all users registered<br>
   # Only for private and admin users
   def list 
     @users=User.all.order(sort_column + " " + sort_direction)
@@ -61,21 +61,22 @@ class HomeController < ApplicationController
   end
 
   ##
-  # Allow to delete users, later ? 
+  # Allow to delete users, to develop ? 
   def destroy
     redirect_to root_url
   end
-
-  # Functions intended for the sortable method in application_helper
+  
+  ##
+  # Functions used by the sortable method in application_helper and by the list method
   private
     ##
-    # Return the sorting direction
+    # Return the name of the column to sort
     def sort_column
       User.column_names.include?(params[:sort]) ? params[:sort] : "id"
     end
   
     ##
-    # Return the name of the column to sort
+    # Return the sorting direction
     def sort_direction
       %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
