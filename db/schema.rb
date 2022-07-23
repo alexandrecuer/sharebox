7dfd3eaeb4c8e33ab649479af1a400a3ada6e7c1
Alexjunk <alexandre.cuer@cerema.fr>
2022-07-23 14:12:21 +0200

    removing paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190727173609) do
-
+ActiveRecord::Schema[7.0].define(version: 2019_07_27_173609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +19,7 @@ ActiveRecord::Schema.define(version: 20190727173609) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -32,18 +31,18 @@ ActiveRecord::Schema.define(version: 20190727173609) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "assets", force: :cascade do |t|
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "uploaded_file_file_name"
     t.string "uploaded_file_content_type"
     t.bigint "uploaded_file_file_size"
-    t.datetime "uploaded_file_updated_at"
+    t.datetime "uploaded_file_updated_at", precision: nil
     t.integer "folder_id"
     t.index ["folder_id"], name: "index_assets_on_folder_id"
     t.index ["user_id"], name: "index_assets_on_user_id"
@@ -52,16 +51,16 @@ ActiveRecord::Schema.define(version: 20190727173609) do
   create_table "clients", force: :cascade do |t|
     t.string "mel"
     t.string "organisation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "folders", force: :cascade do |t|
     t.string "name"
     t.integer "parent_id"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "case_number"
     t.integer "poll_id"
     t.text "lists"
@@ -82,8 +81,8 @@ ActiveRecord::Schema.define(version: 20190727173609) do
   end
 
   create_table "satisfactions", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
     t.string "case_number"
     t.integer "folder_id"
@@ -139,8 +138,8 @@ ActiveRecord::Schema.define(version: 20190727173609) do
     t.integer "share_user_id"
     t.integer "folder_id"
     t.string "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["folder_id"], name: "index_shared_folders_on_folder_id"
     t.index ["share_user_id"], name: "index_shared_folders_on_share_user_id"
     t.index ["user_id"], name: "index_shared_folders_on_user_id"
@@ -154,23 +153,23 @@ ActiveRecord::Schema.define(version: 20190727173609) do
     t.string "description"
     t.string "by"
     t.string "token"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "statut", default: "public"
     t.text "groups"
     t.string "lang"
